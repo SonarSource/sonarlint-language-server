2a0598aa3fea4cc1719bbc8aec706d6cafb21d05
Julien HENRY <julien.henry@sonarsource.com>
2022-03-08 18:05:24 +0100

    Rewrite medium test with workspace folders

--- src/test/java/org/sonarsource/sonarlint/ls/mediumtests/LanguageServerWithFoldersMediumTests.java
+++ src/test/java/org/sonarsource/sonarlint/ls/mediumtests/LanguageServerWithFoldersMediumTests.java
@@ -21,67 +21,55 @@ package org.sonarsource.sonarlint.ls.mediumtests;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.DiagnosticSeverity;
 import org.eclipse.lsp4j.WorkspaceFolder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LanguageServerWithFoldersMediumTests extends AbstractLanguageServerMediumTests {
+
+  @TempDir
+  public static Path folderBaseDir;
 
   @BeforeAll
   public static void initialize() throws Exception {
     var fakeTypeScriptProjectPath = Paths.get("src/test/resources/fake-ts-project").toAbsolutePath();
 
-    client.settingsLatch = new CountDownLatch(1);
-    var folderUri = "file:///init_uri";
-    client.folderSettings.put(folderUri, buildSonarLintSettingsSection("some pattern", null, null, true));
+    client.folderSettings.put(folderBaseDir.toUri().toString(), buildSonarLintSettingsSection("**/*Test.js", null, null, true));
 
     initialize(Map.of(
       "typeScriptLocation", fakeTypeScriptProjectPath.resolve("node_modules").toString(),
       "telemetryStorage", "not/exists",
       "productName", "SLCORE tests",
-      "productVersion", "0.1"
-    ), new WorkspaceFolder(folderUri, "My Folder"));
+      "productVersion", "0.1"), new WorkspaceFolder(folderBaseDir.toUri().toString(), "My Folder"));
 
-    emulateConfigurationChangeOnClient(null, false, false, false);
-
-    assertTrue(client.settingsLatch.await(5, SECONDS));
-  }
-
-  // Override parent to not clear folderSettings
-  @Override
-  public void cleanup() throws InterruptedException {
-    // Wait for logs to stop being produced
-    await().during(1, SECONDS).atMost(5, SECONDS).until(() -> {
-      int count = client.logs.size();
-      client.logs.clear();
-      return count;
-    }, equalTo(0));
   }
 
   @Test
-  void analyzeSimpleJsFileOnOpen() throws Exception {
-    emulateConfigurationChangeOnClient("**/*Test.js", true);
+  void analysisShouldUseFolderSettings() throws Exception {
+    // In folder settings, the test pattern is **/*Test.js while in global config we put **/*.js
+    emulateConfigurationChangeOnClient("**/*.js", true);
 
-    var uri = getUri("analyzeSimpleJsFileOnOpen.js");
-    var diagnostics = didOpenAndWaitForDiagnostics(uri, "javascript", "function foo() {\n  var toto = 0;\n  var plouf = 0;\n}");
+    var uriInFolder = folderBaseDir.resolve("inFolder.js").toUri().toString();
+    var diagnosticsInFolder = didOpenAndWaitForDiagnostics(uriInFolder, "javascript", "function foo() {\n  var toto = 0;\n  var plouf = 0;\n}");
 
-    assertThat(diagnostics)
+    assertThat(diagnosticsInFolder)
       .extracting(startLine(), startCharacter(), endLine(), endCharacter(), code(), Diagnostic::getSource, Diagnostic::getMessage, Diagnostic::getSeverity)
       .containsExactly(
         tuple(1, 6, 1, 10, "javascript:S1481", "sonarlint", "Remove the declaration of the unused 'toto' variable.", DiagnosticSeverity.Information),
         tuple(2, 6, 2, 11, "javascript:S1481", "sonarlint", "Remove the declaration of the unused 'plouf' variable.", DiagnosticSeverity.Information));
+
+    var uriOutsideFolder = getUri("outsideFolder.js");
+    var diagnosticsOutsideFolder = didOpenAndWaitForDiagnostics(uriOutsideFolder, "javascript", "function foo() {\n  var toto = 0;\n  var plouf = 0;\n}");
+
+    // File is considered as test file
+    assertThat(diagnosticsOutsideFolder).isEmpty();
   }
 
 }
