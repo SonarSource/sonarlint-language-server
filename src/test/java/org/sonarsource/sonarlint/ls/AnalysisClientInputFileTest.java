331612f6ad7897b6837f1bb1bb65e0d159d4ad29
knize <kirill.knize@sonarsource.com>
2022-03-09 07:32:18 +0100

    fix test

--- src/test/java/org/sonarsource/sonarlint/ls/AnalysisClientInputFileTest.java
+++ src/test/java/org/sonarsource/sonarlint/ls/AnalysisClientInputFileTest.java
@@ -60,8 +60,8 @@ class AnalysisClientInputFileTest {
 
       Arguments.of("php", Language.PHP),
       Arguments.of("java", Language.JAVA),
-      Arguments.of("с", Language.C),
-      Arguments.of("сpp", Language.CPP),
+      Arguments.of("c", Language.C),
+      Arguments.of("cpp", Language.CPP),
 
       Arguments.of("unknown", null)
     );
