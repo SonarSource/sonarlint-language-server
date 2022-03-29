b23d83e7d0bc699eb8757a6e79a10f68418d4300
Damien Urruty <damien.urruty@sonarsource.com>
2022-03-29 11:16:52 +0200

    SLLS-67 Properly shutdown branch manager
    We now use the same single thread executor for all branches resolutions

--- src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
+++ src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
@@ -278,6 +278,7 @@ public class SonarLintLanguageServer implements SonarLintExtendedLanguageServer,
       serverNotifications.shutdown();
       standaloneEngineManager.shutdown();
       moduleEventsProcessor.shutdown();
+      branchManager.shutdown();
       return new Object();
     });
   }
---
+++ src/test/java/org/sonarsource/sonarlint/ls/util/ImmediateExecutorService.java
@@ -0,0 +1,104 @@
+/*
+ * SonarLint Language Server
+ * Copyright (C) 2009-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.ls.util;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ImmediateExecutorService implements ExecutorService {
+  @Override
+  public void shutdown() {
+
+  }
+
+  @Override
+  public List<Runnable> shutdownNow() {
+    return null;
+  }
+
+  @Override
+  public boolean isShutdown() {
+    return false;
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return false;
+  }
+
+  @Override
+  public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+    return false;
+  }
+
+  @Override
+  public <T> Future<T> submit(Callable<T> task) {
+    try {
+      return CompletableFuture.completedFuture(task.call());
+    } catch (Exception e) {
+      return CompletableFuture.failedFuture(e);
+    }
+  }
+
+  @Override
+  public <T> Future<T> submit(Runnable task, T result) {
+    task.run();
+    return CompletableFuture.completedFuture(result);
+  }
+
+  @Override
+  public Future<?> submit(Runnable task) {
+    task.run();
+    return CompletableFuture.completedFuture(null);
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+    return null;
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    return null;
+  }
+
+  @Override
+  public void execute(Runnable command) {
+    command.run();
+  }
+}
