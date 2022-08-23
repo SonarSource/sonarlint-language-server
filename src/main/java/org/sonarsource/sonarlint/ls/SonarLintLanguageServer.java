d1244c8d0c7216ee720dd7bc757c4a7dc1c238ab
Damien Urruty <damien.urruty@sonarsource.com>
2022-08-23 19:41:02 +0200

    SLLS-91 Auto sync issues when switching branch

--- src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
+++ src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
@@ -186,7 +186,7 @@ public class SonarLintLanguageServer implements SonarLintExtendedLanguageServer,
     this.commandManager = new CommandManager(client, settingsManager, bindingManager, serverSynchronizer, telemetry, standaloneEngineManager, taintVulnerabilitiesCache,
       issuesCache);
     this.securityHotspotsHandlerServer = new SecurityHotspotsHandlerServer(lsLogOutput, bindingManager, client, telemetry, settingsManager);
-    this.branchManager = new WorkspaceFolderBranchManager(client, bindingManager);
+    this.branchManager = new WorkspaceFolderBranchManager(client, bindingManager, serverSynchronizer);
     this.bindingManager.setBranchResolver(branchManager::getReferenceBranchNameForFolder);
     this.workspaceFoldersManager.addListener(this.branchManager);
     launcher.startListening();
