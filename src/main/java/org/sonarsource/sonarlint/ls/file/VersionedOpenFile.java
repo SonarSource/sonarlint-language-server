0122d399563d16564d00d10005166f168ec5d71b
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2023-02-06 17:13:28 +0100

    SLLS-116 wip. didOpen issue reporting works

--- src/main/java/org/sonarsource/sonarlint/ls/file/VersionedOpenFile.java
+++ src/main/java/org/sonarsource/sonarlint/ls/file/VersionedOpenFile.java
@@ -32,12 +32,14 @@ public class VersionedOpenFile {
   private final String languageId;
   private final int version;
   private final String content;
+  private final boolean isNotebook;
 
-  public VersionedOpenFile(URI uri, String languageId, int version, String content) {
+  public VersionedOpenFile(URI uri, String languageId, int version, String content, boolean isNotebook) {
     this.uri = uri;
     this.languageId = languageId;
     this.version = version;
     this.content = content;
+    this.isNotebook = isNotebook;
   }
 
   public URI getUri() {
@@ -58,6 +60,10 @@ public class VersionedOpenFile {
 
   public boolean isJava() {
     return "java".equals(languageId);
+  }
+
+  public boolean isNotebook() {
+    return isNotebook;
   }
 
   @Override
