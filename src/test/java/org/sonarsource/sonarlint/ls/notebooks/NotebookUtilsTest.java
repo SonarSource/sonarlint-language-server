1a688343e4e826015edbf7132f99542a80606362
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-02-09 13:24:52 +0100

    SLLS-116 Refactor test

--- src/test/java/org/sonarsource/sonarlint/ls/notebooks/NotebookUtilsTest.java
+++ src/test/java/org/sonarsource/sonarlint/ls/notebooks/NotebookUtilsTest.java
@@ -26,7 +26,7 @@ import org.eclipse.lsp4j.TextDocumentItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class NotebookUtilsTest {
   TextDocumentItem originalCell;
@@ -38,11 +38,10 @@ class NotebookUtilsTest {
       "a = True\n" +
       "b = False");
   }
+
   @Test
   void shouldApplySingleLineChangeOnTheFirstLine() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("a");
-    textChange.setRange(new Range(new Position(0, 1), new Position(0, 4)));
+    var textChange = newChange(0, 1, 0, 4, "a");
 
     var changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     var expectedNewContent = "pat(\"hello\")\n" +
@@ -50,14 +49,12 @@ class NotebookUtilsTest {
       "a = True\n" +
       "b = False";
 
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
   }
 
   @Test
   void shouldApplySingleLineChangeOnTheSecondLine() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("c = 42");
-    textChange.setRange(new Range(new Position(1, 0), new Position(1, 0)));
+    var textChange = newChange(1, 0, 1, 0, "c = 42");
 
     var changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     var expectedNewContent = "print(\"hello\")\n" +
@@ -65,14 +62,12 @@ class NotebookUtilsTest {
       "a = True\n" +
       "b = False";
 
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
   }
 
   @Test
   void shouldApplySingleLineChangeAtTheEndOfFile() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("oo");
-    textChange.setRange(new Range(new Position(3, 1), new Position(3, 1)));
+    var textChange = newChange(3, 1 ,3, 1, "oo");
 
     var changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     var expectedNewContent = "print(\"hello\")\n" +
@@ -80,27 +75,23 @@ class NotebookUtilsTest {
       "a = True\n" +
       "boo = False";
 
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
   }
 
   @Test
   void shouldApplyMultiLineDeletion() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("");
-    textChange.setRange(new Range(new Position(1, 0), new Position(2, 8)));
+    var textChange = newChange(1, 0, 2, 8, "");
 
     var changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     var expectedNewContent = "print(\"hello\")\n" +
       "b = False";
 
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
   }
 
   @Test
   void shouldApplyMultiLineChangeWithEmptyLine() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("\n");
-    textChange.setRange(new Range(new Position(1, 0), new Position(2, 8)));
+    var textChange = newChange(1, 0, 2, 8, "\n");
 
     var changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     var expectedNewContent = "print(\"hello\")\n" +
@@ -108,14 +99,12 @@ class NotebookUtilsTest {
       "\n" +
       "b = False";
 
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
   }
 
   @Test
   void shouldAddAndDeleteLastLine() {
-    TextDocumentContentChangeEvent textChange = new TextDocumentContentChangeEvent();
-    textChange.setText("\n");
-    textChange.setRange(new Range(new Position(3, 9), new Position(3, 9)));
+    var textChange = newChange(3, 9, 3, 9, "\n");
 
     String changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     String expectedNewContent = "print(\"hello\")\n" +
@@ -123,17 +112,23 @@ class NotebookUtilsTest {
       "a = True\n" +
       "b = False" +
       "\n";
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
     originalCell.setText(changedContent);
 
-    textChange.setText("");
-    textChange.setRange(new Range(new Position(3, 9), new Position(4, 0)));
+    textChange = newChange(3, 9, 4, 0, "");
 
     changedContent = NotebookUtils.applyChangeToCellContent(originalCell, textChange);
     expectedNewContent = "print(\"hello\")\n" +
       "\n" +
       "a = True\n" +
       "b = False";
-    assertEquals(changedContent, expectedNewContent);
+    assertThat(changedContent).isEqualTo(expectedNewContent);
+  }
+
+  private static TextDocumentContentChangeEvent newChange(int startLine, int startLineOffset, int endLine, int endLineOffset, String replacement) {
+    var textChange = new TextDocumentContentChangeEvent();
+    textChange.setRange(new Range(new Position(startLine, startLineOffset), new Position(endLine, endLineOffset)));
+    textChange.setText(replacement);
+    return textChange;
   }
 }
