6724aeb5f59bba2eb8b6788933a8b8ce5b8d9d84
Knize <kirill.knize@sonarsource.com>
2023-08-29 10:43:57 +0200

    remove import

--- src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
+++ src/main/java/org/sonarsource/sonarlint/ls/SonarLintLanguageServer.java
@@ -27,7 +27,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Collection;
