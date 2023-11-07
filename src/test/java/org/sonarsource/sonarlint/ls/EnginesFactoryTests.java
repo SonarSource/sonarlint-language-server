c66e7499a0cec0b49d7afbfed2e4aaad49ad249d
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-11-07 14:21:18 +0100

    SLLS-190 Analyze ARM templates and Bicep files

--- src/test/java/org/sonarsource/sonarlint/ls/EnginesFactoryTests.java
+++ src/test/java/org/sonarsource/sonarlint/ls/EnginesFactoryTests.java
@@ -51,6 +51,7 @@ class EnginesFactoryTests {
   @Test
   void get_standalone_languages() {
     assertThat(EnginesFactory.getStandaloneLanguages()).containsExactlyInAnyOrder(
+      Language.AZURERESOURCEMANAGER,
       Language.C,
       Language.CLOUDFORMATION,
       Language.CS,
