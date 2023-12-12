dc39a449e203d096cd2e80cc439a524e3e496939
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2023-12-12 15:37:36 +0100

    SLLS-198 allow users to mute node runtime requirement notification

--- src/main/java/org/sonarsource/sonarlint/ls/SkippedPluginsNotifier.java
+++ src/main/java/org/sonarsource/sonarlint/ls/SkippedPluginsNotifier.java
@@ -29,15 +29,19 @@ import org.eclipse.lsp4j.MessageType;
 import org.eclipse.lsp4j.ShowMessageRequestParams;
 import org.sonarsource.sonarlint.core.analysis.api.AnalysisResults;
 import org.sonarsource.sonarlint.core.client.api.common.PluginDetails;
+import org.sonarsource.sonarlint.core.commons.log.SonarLintLogger;
 import org.sonarsource.sonarlint.core.plugin.commons.SkipReason;
 
 import static java.util.stream.Collectors.toSet;
+import static org.sonarsource.sonarlint.core.plugin.commons.SkipReason.UnsatisfiedRuntimeRequirement.RuntimeRequirement.JRE;
+import static org.sonarsource.sonarlint.core.plugin.commons.SkipReason.UnsatisfiedRuntimeRequirement.RuntimeRequirement.NODEJS;
 
 public class SkippedPluginsNotifier {
 
   private final Set<String> displayedMessages = new HashSet<>();
 
   public static final MessageActionItem ACTION_OPEN_SETTINGS = new MessageActionItem("Open Settings");
+  public static final MessageActionItem ACTION_DONT_SHOW_AGAIN = new MessageActionItem("Don't Show Again");
 
   private final SonarLintExtendedLanguageClient client;
 
@@ -53,34 +57,53 @@ public class SkippedPluginsNotifier {
     attemptedLanguages.forEach(l -> {
       final var correspondingPlugin = allPlugins.stream().filter(p -> p.key().equals(l.getPluginKey())).findFirst();
       correspondingPlugin.flatMap(PluginDetails::skipReason).ifPresent(skipReason -> {
-        if (skipReason instanceof SkipReason.UnsatisfiedRuntimeRequirement) {
-          final var runtimeRequirement = (SkipReason.UnsatisfiedRuntimeRequirement) skipReason;
+        if (skipReason instanceof SkipReason.UnsatisfiedRuntimeRequirement runtimeRequirement) {
           final var title = String.format("SonarLint failed to analyze %s code", l.getLabel());
-          if (runtimeRequirement.getRuntime() == SkipReason.UnsatisfiedRuntimeRequirement.RuntimeRequirement.JRE) {
-            var content = String.format(
-              "Java runtime version %s or later is required. Current version is %s.", runtimeRequirement.getMinVersion(), runtimeRequirement.getCurrentVersion());
-            showMessageWithOpenSettingsAction(client, formatMessage(title, content), client::openJavaHomeSettings);
-          } else if (runtimeRequirement.getRuntime() == SkipReason.UnsatisfiedRuntimeRequirement.RuntimeRequirement.NODEJS) {
-            var content = String.format(
-              "Node.js runtime version %s or later is required.", runtimeRequirement.getMinVersion());
-            if (runtimeRequirement.getCurrentVersion() != null) {
-              content += String.format(" Current version is %s.", runtimeRequirement.getCurrentVersion());
-            }
-            showMessageWithOpenSettingsAction(client, formatMessage(title, content), client::openPathToNodeSettings);
+          if (runtimeRequirement.getRuntime() == JRE) {
+            handleMissingJRERequirement(runtimeRequirement, title);
+          } else if (runtimeRequirement.getRuntime() == NODEJS) {
+            handleMissingNodeRuntimeRequirement(runtimeRequirement, title);
           }
         }
       });
     });
   }
 
-  private void showMessageWithOpenSettingsAction(SonarLintExtendedLanguageClient client, String message, Runnable callback) {
+  private void handleMissingJRERequirement(SkipReason.UnsatisfiedRuntimeRequirement runtimeRequirement, String title) {
+    var content = String.format(
+      "Java runtime version %s or later is required. Current version is %s.", runtimeRequirement.getMinVersion(), runtimeRequirement.getCurrentVersion());
+    SonarLintLogger.get().warn(content);
+    showMessageWithOpenSettingsAction(client, formatMessage(title, content), JRE,
+      client::openJavaHomeSettings);
+  }
+
+  private void handleMissingNodeRuntimeRequirement(SkipReason.UnsatisfiedRuntimeRequirement runtimeRequirement, String title) {
+    var content = String.format(
+      "Node.js runtime version %s or later is required.", runtimeRequirement.getMinVersion());
+    if (runtimeRequirement.getCurrentVersion() != null) {
+      content += String.format(" Current version is %s.", runtimeRequirement.getCurrentVersion());
+    }
+    var isNotificationAllowed = client.canShowMissingRequirementsNotification().join();
+    SonarLintLogger.get().warn(content);
+    if (Boolean.TRUE.equals(isNotificationAllowed)) {
+      showMessageWithOpenSettingsAction(client, formatMessage(title, content), NODEJS,
+        client::openPathToNodeSettings);
+    }
+  }
+
+  private void showMessageWithOpenSettingsAction(SonarLintExtendedLanguageClient client, String message,
+    SkipReason.UnsatisfiedRuntimeRequirement.RuntimeRequirement requirementType, Runnable callback) {
     if (displayedMessages.add(message)) {
-      var params = new ShowMessageRequestParams(List.of(ACTION_OPEN_SETTINGS));
+      var params = requirementType == NODEJS ?
+        new ShowMessageRequestParams(List.of(ACTION_OPEN_SETTINGS, ACTION_DONT_SHOW_AGAIN))
+        : new ShowMessageRequestParams(List.of(ACTION_OPEN_SETTINGS));
       params.setType(MessageType.Error);
       params.setMessage(message);
       client.showMessageRequest(params).thenAccept(action -> {
         if (ACTION_OPEN_SETTINGS.equals(action)) {
           callback.run();
+        } else if (ACTION_DONT_SHOW_AGAIN.equals(action)) {
+          client.doNotShowMissingRequirementsMessageAgain();
         }
       });
     }
