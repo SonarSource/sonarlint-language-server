425016f6212a7c815b966392b8c6a9327bd27eef
Knize <kirill.knize@sonarsource.com>
2024-02-29 17:40:14 +0100

    remove import

--- src/main/java/org/sonarsource/sonarlint/ls/AnalysisScheduler.java
+++ src/main/java/org/sonarsource/sonarlint/ls/AnalysisScheduler.java
@@ -32,7 +32,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import org.sonarsource.sonarlint.ls.connected.ProjectBindingManager;
