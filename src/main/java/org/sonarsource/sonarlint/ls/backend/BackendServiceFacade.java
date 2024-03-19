725001ae3eff1052d8929db46455bda8aaee5a62
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2024-03-19 16:26:00 +0100

    SLLS-229 Move telemetry kill switch system property from core to language server

--- src/main/java/org/sonarsource/sonarlint/ls/backend/BackendServiceFacade.java
+++ src/main/java/org/sonarsource/sonarlint/ls/backend/BackendServiceFacade.java
@@ -51,6 +51,7 @@ import org.sonarsource.sonarlint.ls.SonarLintExtendedLanguageClient;
 import org.sonarsource.sonarlint.ls.log.LanguageClientLogger;
 import org.sonarsource.sonarlint.ls.settings.ServerConnectionSettings;
 import org.sonarsource.sonarlint.ls.settings.SettingsManager;
+import org.sonarsource.sonarlint.ls.telemetry.SonarLintTelemetry;
 import org.sonarsource.sonarlint.ls.telemetry.TelemetryInitParams;
 
 public class BackendServiceFacade {
@@ -66,6 +67,7 @@ public class BackendServiceFacade {
   private SettingsManager settingsManager;
   private TelemetryInitParams telemetryInitParams;
   private final CountDownLatch initLatch = new CountDownLatch(1);
+  private SonarLintTelemetry telemetry;
 
   public BackendServiceFacade(SonarLintRpcClientDelegate rpcClient, LanguageClientLogger lsLogOutput, SonarLintExtendedLanguageClient client) {
     this.lsLogOutput = lsLogOutput;
@@ -125,6 +127,7 @@ public class BackendServiceFacade {
   }
 
   private InitializeParams toInitParams(BackendInitParams initParams) {
+    var telemetryEnabled = telemetry != null && telemetry.enabled();
     return new InitializeParams(
       new ClientConstantInfoDto("Visual Studio Code", initParams.getUserAgent()),
       new TelemetryClientConstantAttributesDto(initParams.getTelemetryProductKey(),
@@ -135,7 +138,7 @@ public class BackendServiceFacade {
       getHttpConfiguration(),
       getSonarCloudAlternativeEnvironment(),
       new FeatureFlagsDto(true, true, true,
-        true, initParams.isEnableSecurityHotspots(), true, true, true),
+        true, initParams.isEnableSecurityHotspots(), true, true, true, telemetryEnabled),
       initParams.getStorageRoot(),
       Path.of(initParams.getSonarlintUserHome()),
       initParams.getEmbeddedPluginPaths(),
@@ -217,5 +220,9 @@ public class BackendServiceFacade {
 
   public CountDownLatch getInitLatch() {
     return initLatch;
+  }
+
+  public void setTelemetry(SonarLintTelemetry telemetry) {
+    this.telemetry = telemetry;
   }
 }
