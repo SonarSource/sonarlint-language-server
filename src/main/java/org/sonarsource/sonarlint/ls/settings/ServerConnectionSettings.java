3dbff0471fa824be6c60e7df7ea349126d6b1234
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-03-11 10:53:48 +0100

    SLLS-311 change US SonarQube Cloud URL

--- src/main/java/org/sonarsource/sonarlint/ls/settings/ServerConnectionSettings.java
+++ src/main/java/org/sonarsource/sonarlint/ls/settings/ServerConnectionSettings.java
@@ -38,7 +38,7 @@ public class ServerConnectionSettings {
   public static final String SONARCLOUD_URL = System.getProperty("sonarlint.internal.sonarcloud.url") != null ?
     System.getProperty("sonarlint.internal.sonarcloud.url") :
     "https://sonarcloud.io";
-  public static final String SONARCLOUD_US_URL = "https://us.sonarcloud.io/";
+  public static final String SONARCLOUD_US_URL = "https://sonarqube.us/";
   static final String[] SONARCLOUD_ALIAS = {"https://sonarqube.com", "https://www.sonarqube.com", "https://www.sonarcloud.io", SONARCLOUD_URL, SONARCLOUD_US_URL};
 
   private final String connectionId;
