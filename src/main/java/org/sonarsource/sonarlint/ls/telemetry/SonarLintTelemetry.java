8f37f99288514f9ea6dc0030bb284273d17992b1
Kirill Knize <kirill.knize@sonarsource.com>
2025-06-23 10:51:35 +0200

    PR feedback

--- src/main/java/org/sonarsource/sonarlint/ls/telemetry/SonarLintTelemetry.java
+++ src/main/java/org/sonarsource/sonarlint/ls/telemetry/SonarLintTelemetry.java
@@ -75,13 +75,17 @@ public class SonarLintTelemetry implements WorkspaceSettingsChangeListener {
   }
 
   public void taintVulnerabilitiesInvestigatedLocally() {
-    actIfEnabled(TelemetryRpcService::taintVulnerabilitiesInvestigatedLocally);
-    actIfEnabled(TelemetryRpcService::taintInvestigatedLocally);
+    actIfEnabled(telemetryRpcService -> {
+      telemetryRpcService.taintVulnerabilitiesInvestigatedLocally();
+      telemetryRpcService.taintInvestigatedLocally();
+    });
   }
 
   public void taintVulnerabilitiesInvestigatedRemotely() {
-    actIfEnabled(TelemetryRpcService::taintVulnerabilitiesInvestigatedRemotely);
-    actIfEnabled(TelemetryRpcService::taintInvestigatedRemotely);
+    actIfEnabled(telemetryRpcService -> {
+      telemetryRpcService.taintVulnerabilitiesInvestigatedRemotely();
+      telemetryRpcService.taintInvestigatedRemotely();
+    });
   }
 
   public void addQuickFixAppliedForRule(String ruleKey) {
