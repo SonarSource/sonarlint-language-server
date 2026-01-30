ca7fc934f39cfe6f373dbb4c3613f366c4f5e744
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2026-01-30 11:30:49 +0100

    Remove misconfigured assertions on the config scope setup

--- src/test/java/org/sonarsource/sonarlint/ls/mediumtests/ConnectedModeMediumTests.java
+++ src/test/java/org/sonarsource/sonarlint/ls/mediumtests/ConnectedModeMediumTests.java
@@ -599,11 +599,7 @@ class ConnectedModeMediumTests extends AbstractLanguageServerMediumTests {
   private void addConfigScope(String configScopeId) {
     addFolder(configScopeId, Path.of(URI.create(configScopeId)).getFileName().toString());
     awaitUntilAsserted(() -> assertThat(client)
-      .satisfiesAnyOf(
-        c -> assertThat(c.newCodeDefinitionCache).containsKey(configScopeId),
-        c -> assertThat(c.logs.stream()).anyMatch(messageParams -> messageParams.getMessage().contains(String.format("Configuration scope '%s' is already bound", configScopeId))),
-        c -> assertThat(c.logs.stream())
-          .anyMatch(messageParams -> messageParams.getMessage().contains(String.format("Duplicate configuration scope registered: %s", configScopeId)))));
+      .satisfiesAnyOf(c -> assertThat(c.newCodeDefinitionCache).containsKey(configScopeId)));
   }
 
   @Test
